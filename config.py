71bd4692322017f37645ee81b8145856fcc18b8a
Ravikiran Bhonagiri <bhonagiri.ravikiran@gmail.com>
2024-11-21 09:32:17 -0600

    add files

---
+++ config.py
@@ -0,0 +1,78 @@
+# Heart Rate Features
+heart_rate_features_1 = [
+    'HeartRate' 
+]
+
+heart_rate_features_2 = [
+    'HeartRate_15_Mean', 'HeartRate_15_Std', 
+    'HeartRate_30_Mean', 'HeartRate_30_Std', 
+    'HeartRate_60_Mean', 'HeartRate_60_Std', 
+    'HeartRate_90_Mean', 'HeartRate_90_Std', 
+    'HeartRate_180_Mean', 'HeartRate_180_Std', 
+    'HeartRate_240_Mean', 'HeartRate_240_Std', 
+    'HeartRate_360_Mean', 'HeartRate_360_Std', 
+    'HeartRate_720_Mean', 'HeartRate_720_Std', 
+    'HeartRate_1440_Mean', 'HeartRate_1440_Std',
+]
+
+
+# Sleep Features
+
+sleep_features_1 = [
+    'Sleep'
+]
+
+sleep_features_2 = [
+     'Sleep_15min_Mean', 'Sleep_15min_Std',
+    'Sleep_30min_Mean', 'Sleep_30min_Std',
+    'Sleep_60min_Mean', 'Sleep_60min_Std',
+    'Sleep_90min_Mean', 'Sleep_90min_Std',
+    'Sleep_180min_Mean', 'Sleep_180min_Std',
+    'Sleep_240min_Mean', 'Sleep_240min_Std',
+    'Sleep_360min_Mean', 'Sleep_360min_Std',
+    'Sleep_720min_Mean', 'Sleep_720min_Std',
+    'Sleep_1440min_Mean', 'Sleep_1440min_Std',
+]
+
+
+# Intensity Features
+
+intensity_features_1 = [
+    'Intensity', 
+]
+
+intensity_features_2 = [
+    'Intensity_15min_Mean', 'Intensity_15min_Std', 
+    'Intensity_30min_Mean', 'Intensity_30min_Std', 
+    'Intensity_60min_Mean', 'Intensity_60min_Std',  
+    'Intensity_90min_Mean', 'Intensity_90min_Std', 
+    'Intensity_180min_Mean', 'Intensity_180min_Std', 
+    'Intensity_240min_Mean', 'Intensity_240min_Std', 
+    'Intensity_360min_Mean', 'Intensity_360min_Std',
+    'Intensity_720min_Mean', 'Intensity_720min_Std', 
+    'Intensity_1440min_Mean', 'Intensity_1440min_Std',
+]
+
+
+# Steps Features
+
+steps_features_1 = [
+    'Steps', 
+]
+
+steps_features_2 = [
+    'Steps_15min_Mean', 'Steps_15min_Std', 'Steps_30min_Mean', 'Steps_30min_Std', 
+    'Steps_60min_Mean', 'Steps_60min_Std', 'Steps_90min_Mean', 'Steps_90min_Std', 
+    'Steps_180min_Mean', 'Steps_180min_Std', 'Steps_240min_Mean', 'Steps_240min_Std', 
+    'Steps_360min_Mean', 'Steps_360min_Std', 'Steps_720min_Mean', 'Steps_720min_Std', 
+    'Steps_1440min_Mean', 'Steps_1440min_Std',
+]
+
+
+temporal_features = [
+     'MinuteOfDay_Sin', 
+     'MinuteOfDay_Cos',
+     'DayOfWeek_Sin',
+     'DayOfWeek_Cos',
+     'Is_Weekend'
+]
